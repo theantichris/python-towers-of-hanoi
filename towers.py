6e9ae7c4f4cc1ed95b51aca394eaa9dd94d5277e
Christopher Lamm <chris@theantichris.com>
2019-12-04 13:48:34 -0500

    setting up the game

--- towers.py
+++ towers.py
@@ -1,3 +1,15 @@
 from stack import Stack
 
 print ("\nLet's play Towers of Hanoi!")
+
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
+
+num_disks = int(input("\nHow many disks do you want to play with: "))
+while num_disks < 3:
+    num_disks = int(input("Enter a number greater than or equal to 3: "))
